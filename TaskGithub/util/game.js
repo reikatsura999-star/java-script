179f0e2c1ab964c96983226056a987d3d7a64c4a
Ikhya <reikatsura@gmail.com>
2025-11-14 08:00:25 +0700

    answer question

--- TaskGithub/util/game.js
+++ TaskGithub/util/game.js
@@ -1,14 +1,4 @@
 
-// export const POINT_MAXIMUM = 1000
-// export const POIN_BONUS = 50
-
-// export default function hitungTotalPoin (nilai){ 
-//     const total = nilai.reduce((acc,val)=> acc + val,0)
-//     const hasil = total + POIN_BONUS;
-//     return hasil > POINT_MAXIMUM ? POINT_MAXIMUM : hasil
-// }
-
-// game.js
 export const POIN_MAKSIMAL = 1000;
 export const  POIN_BONUS = 50;
 
